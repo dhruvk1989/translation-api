4022ce439d7894aed486bf7bc4d110b84da52fb8
dhruvk1989 <79693033+dhruvk1989@users.noreply.github.com>
2022-05-16 15:55:16 +0530

    Update ObjectFactory.java

--- src/main/java/com/dhruv/translationapi/util/ObjectFactory.java
+++ src/main/java/com/dhruv/translationapi/util/ObjectFactory.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class ObjectFactory {
     private final String BASE_URL = "https://translation.googleapis.com/language/translate/v2";
-    private final String KEY = "YOUR API KEY NEEDS TO COME HERE";
+    private final String KEY = "YOUR API KEY NEEDS TO COME HERE and i don't care.";
 
     public String getKEY() {
         return KEY;
