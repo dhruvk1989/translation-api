29706ed528fba8e4d0eb98329653960e8813de0e
dhruvk1989 <dhruvachintya22@gmail.com>
2022-05-18 17:41:13 +0530

    Set up translator, translation requests and requester tables

--- src/main/java/com/dhruv/translationapi/entities/Requests.java
+++ src/main/java/com/dhruv/translationapi/entities/Requests.java
@@ -8,6 +8,33 @@ public class Requests {
     @Column(name = "request_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long requestId;
+    private Long requesterId;
+    private Long translatorId;
+    private Integer status;
+
+    public Long getRequesterId() {
+        return requesterId;
+    }
+
+    public void setRequesterId(Long requesterId) {
+        this.requesterId = requesterId;
+    }
+
+    public Long getTranslatorId() {
+        return translatorId;
+    }
+
+    public void setTranslatorId(Long translatorId) {
+        this.translatorId = translatorId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 
     public Long getRequestId() {
         return requestId;
--- src/main/java/com/dhruv/translationapi/entities/User.java
+++ src/main/java/com/dhruv/translationapi/entities/User.java
@@ -8,6 +8,33 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id", nullable = false)
     private Long userId;
+    private String username;
+    private String emailAddress;
+    private String fullName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
     public Long getId() {
         return userId;
